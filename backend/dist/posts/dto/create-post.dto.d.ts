a05b8855518ad32d7d901439934eee52a5018c77
warris-chitou <warrisagbannonde@outlook.com>
2024-12-24 14:53:32 +0100

    Suppression du backend

--- backend/dist/posts/dto/create-post.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreatePostDto {
-    title: string;
-    content: string;
-    published?: boolean;
-    groupId?: string;
-    categoryId?: string;
-    thumbnail?: string;
-}
--- backend/dist/users/dto/create-user.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    password: string;
-    bio?: string;
-    avatar?: string;
-}
